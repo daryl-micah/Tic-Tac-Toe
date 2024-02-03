e6907a27249e0ab2456a27bcf49b1bec7175e95f
Daryl Micah <80580135+daryl-micah@users.noreply.github.com>
2024-02-03 10:08:09 +0530

    Added all files

---
+++ app.js
@@ -0,0 +1,90 @@
+let boxes = document.querySelectorAll(".box");
+let resetBtn = document.querySelector("#reset-btn");
+let newGame = document.querySelector("#new-btn");
+let msgContainer = document.querySelector(".msg-container");
+let msg = document.querySelector("#msg");
+
+let turnO = true; //playerX, playerO
+let count = 0;
+
+const winPatterns = [
+    [0, 1, 2],
+    [0, 3, 6],
+    [0, 4, 8],
+    [1, 4, 7],
+    [2, 5, 8],
+    [2, 4, 6],
+    [3, 4, 5],
+    [6, 7, 8]
+];
+
+boxes.forEach((box) => {
+    box.addEventListener("click", () => {
+      if (turnO) {
+        //playerO
+        box.innerText = "O";
+        turnO = false;
+      } else {
+        //playerX
+        box.innerText = "X";
+        turnO = true;
+      }
+      box.disabled = true;
+      count++;
+  
+      let isWinner = checkWinner();
+  
+      if (count === 9 && !isWinner) {
+        gameDraw();
+      }
+    });
+  });
+
+const enableBoxes = () => {
+    for(let box of boxes) {
+        box.disabled = false;
+        box.innerText = "";
+    }
+};
+
+const disableBoxes = () => {
+    for(let box of boxes) {
+        box.disabled = true;
+    }
+};
+
+const resetGame = () => {
+    turnO = true;
+    count = 0;
+    enableBoxes();
+    msgContainer.classList.add("hide");
+}
+
+const showWinner = (winner) => {
+    msg.innerText = `Winner is ${winner}!`;
+    msgContainer.classList.remove("hide");
+    disableBoxes();
+}
+
+const checkWinner = () => {
+    for(let pattern of winPatterns){
+        let pos1 = boxes[pattern[0]].innerText;
+        let pos2 = boxes[pattern[1]].innerText;
+        let pos3 = boxes[pattern[2]].innerText;
+
+        if(pos1 != "" && pos2 != "" && pos3 != ""){
+            if(pos1 === pos2 && pos2 === pos3){
+                console.log("winner ", pos1);
+                showWinner(pos1);
+            }
+        }
+    }
+};
+
+const gameDraw = () => {
+    msg.innerText = `Draw :(`;
+    msgContainer.classList.remove("hide");
+    disableBoxes();
+}
+
+resetBtn.addEventListener("click", resetGame);
